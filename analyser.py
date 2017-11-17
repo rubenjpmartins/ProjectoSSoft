0d5a59a8151e7242878e5c7f051a2afc5dd94ad7
João Freitas <joaotavaresfreitas@hotmail.com>
2017-11-17 15:28:13 +0000

    Add files via upload
    begining of analyser

---
+++ analyser.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+import sys
+
+
+def analyzer(filename):
+	f = open(filename,"r")
+	contents = f.read()
+	f.close()
+	print contents 
+
+if __name__ == '__main__':
+	path = "proj-slices/proj-slices/"
+	filename = path + sys.argv[1]
+	analyzer(filename)
