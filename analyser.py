366dc6489ed2a0d1f0b9cf012993662ab48cb9ac
rubenjpmartins <rubenjpmartins@gmail.com>
2017-11-17 21:09:23 +0000

    patterns not ok

--- analyser.py
+++ analyser.py
@@ -1,5 +1,118 @@
 #!/usr/bin/python
 import sys
+import json
+
+
+#patterns File
+PatternsFile = "patterns.txt"
+
+#patterns List
+Patterns = []
+
+
+# Receives a patterns file and puts the patterns in a data structure
+def getPatternsFile(filename):
+
+
+	patternsFile = open(filename,"r")
+	fileLines = patternsFile.readlines() 
+
+
+	#parameter counter
+	i=0
+	patternsDict = {}
+
+	for line in fileLines:
+
+		if line in ['\n']:
+			i=0
+			continue
+
+		else:
+			#GET vulnerabilities	
+			if i==0:
+				patternsDict["vulnerabilities"] = line
+				
+				i += 1
+				
+			#GET entry_points	
+			if i==1:
+				patternsDict["entry_points"] = tuple(line.rstrip().split(','))
+				
+				i += 1
+
+			#GET sanitization
+			if i==2:
+				patternsDict["sanitization"] = tuple(line.rstrip().split(','))
+			
+				i += 1
+
+
+			#GET sensitive_sinks
+			if i==3:
+			  	patternsDict["sensitive_sinks"] = tuple(line.rstrip().split(','))
+			  	
+			  	print patternsDict
+				i += 1
+
+		
+
+
+		'''
+		#removes blank line and resets parameter counter
+		if line in ['\n']:
+			i=0
+			Patterns.append(patternsDict)
+			patternsDict = {}
+			continue
+		
+
+		#GET vulnerabilities	
+		if i==0:
+			patternsDict["vulnerabilities"] = line
+			print i
+			i += 1
+			
+
+		#GET entry_points	
+		if i==1:
+			patternsDict["entry_points"] = tuple(line.rstrip().split(','))
+			print i
+			i += 1
+
+		#GET sanitization
+		if i==2:
+			patternsDict["sanitization"] = tuple(line.rstrip().split(','))
+			print i
+			i += 1
+
+
+		#GET sensitive_sinks
+		if i==3:
+		  	patternsDict["sensitive_sinks"] = tuple(line.rstrip().split(','))
+		  	print i
+		  	print patternsDict
+			i += 1
+
+		'''
+
+	patternsFile.close()
+
+	print Patterns
+
+
+
+
+
+
+
+
+	
+
+
+
+
+
 
 
 def analyzer(filename):
@@ -9,6 +122,13 @@ def analyzer(filename):
 	print contents 
 
 if __name__ == '__main__':
+
+	#read patterns file with the PatternsFile
+	getPatternsFile(PatternsFile)
+
+'''
 	path = "proj-slices/"
 	filename = path + sys.argv[1]
 	analyzer(filename)
+'''
+
