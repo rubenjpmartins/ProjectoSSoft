de892957cfa6079c19609ef73688397196b6368a
João Freitas <joaotavaresfreitas@hotmail.com>
2017-11-18 17:27:17 +0000

    Update analyser.py

--- analyser.py
+++ analyser.py
@@ -2,13 +2,13 @@
 import sys
 import json
 
-
 #patterns File
 PatternsFile = "patterns.txt"
 
 #patterns List
 Patterns = []
 
+VulnerableVariables = {}
 
 # Receives a patterns file and puts the patterns in a data structure
 def getPatternsFile(filename):
@@ -54,24 +54,64 @@ def getPatternsFile(filename):
 
 			
 	return Patterns
-		
+
+def checkPattern(children, pattern, VulnerableVariables):
+	for i in xrange(0,len(children)):
+		VulnerableVariables = checkVulnerableVariable(children[i], pattern, VulnerableVariables)
+
+		print VulnerableVariables
+
+def checkVulnerableVariable(line, pattern, VulnerableVariables):
+
+	if line["kind"] == "assign":
+
+		if line["right"]["kind"] == "offsetlookup":
+			if line["right"]["what"]["name"] in pattern["entry_points"]:
+					VulnerableVariables[line["left"]["name"]] = line["right"]["what"]["name"]
+
+		elif line["right"]["kind"] == "encapsed":
+			for j in line["right"]["value"]:
+				if j["kind"] == "variable":
+					if j["name"] in VulnerableVariables:
+						VulnerableVariables[line["left"]["name"]] = VulnerableVariables[j["name"]]
+
+		elif line["right"]["kind"] == "call":
+			VulnerableVariables = checkArguments(line["left"]["name"], line["right"], pattern, VulnerableVariables)
+
+	return VulnerableVariables
+
+def checkArguments(possibleVuln, line, pattern, VulnerableVariables):
+	if line["what"]["name"] not in pattern["sanitization"]:
+		for i in line["arguments"]:
+			if i["kind"] == "call":
+				prevSize = len(VulnerableVariables)
+				VulnerableVariables = checkArguments(possibleVuln, i, pattern, VulnerableVariables)
+				if len(VulnerableVariables) > prevSize:
+					return VulnerableVariables
+
+			elif i["kind"] == "variable":
+				if i["name"] in VulnerableVariables:
+					VulnerableVariables[possibleVuln] = VulnerableVariables[i["name"]]
+					return VulnerableVariables
+	else:
+		if possibleVuln in VulnerableVariables:
+			VulnerableVariables.pop(possibleVuln)
+	return VulnerableVariables
 
 
-
-def analyzer(filename):
-	f = open(filename,"r")
-	contents = f.read()
-	f.close()
-	print contents 
+def analyzer(ast):
+	children = ast["children"]
+	VulnerableVariables = {}
+	for pattern in Patterns:
+		checkPattern(children, pattern, VulnerableVariables)
+		VulnerableVariables = {}
 
 if __name__ == '__main__':
-
 	#read patterns file with the PatternsFile
 	getPatternsFile(PatternsFile)
-
-'''
 	path = "proj-slices/"
 	filename = path + sys.argv[1]
-	analyzer(filename)
-'''
-
+	f = open(filename,"r")
+	ast = json.load(f)
+	f.close()
+	analyzer(ast)
